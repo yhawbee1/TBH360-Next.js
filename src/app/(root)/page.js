be942c3911134874d193a6bea8f9bd0d8a347326
Yhawbee1 <antwibrian82@gmail.com>
2024-05-16 19:49:51 +0000

    Add dark mode switcher

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/HeroSection";
+import ThemeSwitch from "@/components/ThemeSwitcher";
 
 
 export default function Home() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import {AiOutlineMenu} from "react-icons/ai"
 import MobileSidebar from "./MobileSidebar";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import ThemeSwitch from "./ThemeSwitcher";
 
 export default function Header() {
 
@@ -35,13 +36,14 @@ export default function Header() {
 
 
   return (
-    <div className="shadow-md fixed w-screen bg-white z-50 px-5">
+    <div className="shadow-md fixed w-screen bg-white dark:bg-tbhgreen z-50 px-5">
       <div className="flex mx-auto justify-between  items-center align-middle ">
         <div>
           <Image src="/images/logo.png" width={80} height={30} alt="alt" />
         </div>
-        <div className="lg:hidden block ">
-          <MobileSidebar Links={navMenuItems}  />
+        <div className="lg:hidden flex items-center gap-2">
+          <MobileSidebar Links={navMenuItems} />
+          <ThemeSwitch  />
         </div>
         <div className="lg:flex hidden ">
           <ul className="flex gap-9">
@@ -55,20 +57,21 @@ export default function Header() {
             ))}
           </ul>
         </div>
-        <div className="gap-4 lg:flex hidden ">
+        <div className="gap-4 lg:flex hidden items-center">
           <div className="border border-[#05C1EF] flex items-center px-2">
             <input
               type="text"
               placeholder="Explore"
-              className="border-none focus:ring-0"
+              className="border-none focus:ring-0 bg-transparent"
             />
             <button>
               <Search className="text-#05C1EF" />
             </button>
           </div>
-          <button className="bg-[#05C1EF]  px-3 text-white rounded-full cursor-pointer hover:bg-[#4aaac2]">
+          <button className="bg-[#05C1EF]  px-4 py-2.5 text-white rounded-full cursor-pointer hover:bg-[#4aaac2]">
             Sign Up
           </button>
+          <ThemeSwitch  />
         </div>
       </div>
     </div>
