50270c1944489df7e4f37ba82f074426dbabac40
Sheldon Debra <kofisheldon@gmail.com>
2024-05-16 19:11:20 +0000

    add

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection";
+import VideoCarousel from "@/components/VideoCarousel";
 
 
 export default function Home() {
   return (
     <main className="">
       <HeroSection />
+      <VideoCarousel/>
     </main>
   );
 }
