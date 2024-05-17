ac5fe0c86a8c6847ef017ae9cf46f5939fcd517f
Sheldon Debra <kofisheldon@gmail.com>
2024-05-17 16:58:06 +0000

    fix footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,14 @@ import { FaXTwitter } from "react-icons/fa6";
 import { IoLogoFacebook } from "react-icons/io5";
 import { FaInstagram } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <>
-      <div className="bg-tbhgreen py-5 text-white">
-        <div className="flex flex-col md:flex-row justify-evenly mt-24 h-auto pb-5 space-y-5 md:space-y-0">
-          <div className="w-full md:w-[20%] px-4 md:px-0">
+      <div className="bg-tbhgreen py-5 text-white ">
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 mx-auto  mt-24 h-auto pb-5 space-y-5 md:space-y-5 md:gap-4  w-[70%]">
+          <div className="  px-4 md:px-0 ">
             <Image src="/images/logo.png" width={100} height={30} alt="alt" />
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam
@@ -23,7 +24,7 @@ export default function Footer() {
               <FaYoutube className="hover:text-tbhblue cursor-pointer" />
             </div>
           </div>
-          <div className="w-full md:w-auto px-4 md:px-0">
+          <div className="w-full md:w-auto px-4 md:px-0 ">
             <h1 className="text-xl font-bold text-white mb-3">Useful Links</h1>
             <div className="text-white space-y-3">
               <p>Account</p>
@@ -32,16 +33,17 @@ export default function Footer() {
               <p>Legal Notice</p>
             </div>
           </div>
-          <div className="w-full md:w-auto px-4 md:px-0">
+          <div className="w-full md:w-auto px-4 md:px-0 ">
             <h1 className="text-xl font-medium text-white mb-3">Other Links</h1>
-            <div className="text-white space-y-3">
-              <p>Sign Up</p>
-              <p>Login</p>
-              <p>About Company</p>
-              <p>Cookies</p>
+            <div className="text-white space-y-3 flex-col flex hover:text ">
+              <Link href="/sign-up"> Register</Link>
+              <Link href="/sign-in"> Login </Link>
+              <Link href="/about"> About Company</Link>
+              <Link href="/cookies"> Cookies </Link>
+              
             </div>
           </div>
-          <div className="w-full md:w-[20%] px-4 md:px-0 space-y-4">
+          <div className="w-full  px-4 md:px-0 space-y-4 ">
             <h1 className="text-xl font-medium">Subscribe to our Newsletter</h1>
             <p className="tracking-tight">
               Unlock exclusive updates, insider insights, and exciting news by
@@ -52,7 +54,7 @@ export default function Footer() {
                 type="email"
                 placeholder="Enter your email here"
                 required
-                className="p-2 rounded"
+                className="p-2 rounded bg-transparent"
               />
               <button className="bg-tbhblue p-2 text-white rounded">
                 Subscribe
