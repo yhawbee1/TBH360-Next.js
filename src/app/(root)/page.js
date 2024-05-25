29222fae2337111b1e2821d5219314fd0bdaec93
Sheldon Debra <kofisheldon@gmail.com>
2024-05-25 18:38:15 +0000

    vide_id

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -3,6 +3,8 @@ import HeroSection from "@/components/HeroSection";
 import ThemeSwitch from "@/components/ThemeSwitcher";
 import RecommendedVideos from "@/components/RecommendedVideos";
 import VideoCarousel from "@/components/VideoCarousel";
+import { Suspense } from "react";
+import SkeletonCard from "@/components/SkeletonCard";
 
 
 
@@ -10,7 +12,9 @@ export default function Home() {
   return (
     <main className="">
       <HeroSection />
+      <Suspense fallback={<SkeletonCard/>}>
       <VideoCarousel/>
+      </Suspense>
       <RecommendedVideos/>
       <Episode/>
     </main>
